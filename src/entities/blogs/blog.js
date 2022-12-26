beb343dbeffa377ce7ed2e162ac91d1e306475fa
mrx7355608 <mrx7355608@gmail.com>
2022-12-26 10:59:20 +0500

    wrote missing error messages

--- src/entities/blogs/blog.js
+++ src/entities/blogs/blog.js
@@ -6,24 +6,29 @@ export default function buildBlogEntity({ sanitizeHtml, arrayOfStringsOnly }) {
         }
         title = sanitizeHtml(title);
         if (title.length < 8) {
-            throw new Error();
+            throw new Error(
+                "Title too short, should be 8 characters long at least"
+            );
         }
 
         // Body
         if (!body) {
             throw new Error("Blog body is required!");
         }
+        body = sanitizeHtml(body);
+        console.log(body);
 
         // Tags
         if (!tags) {
             throw new Error("Cannot create a blog without tags");
         }
         tags = tags.map((tag) => sanitizeHtml(tag));
+        tags = tags.filter((tag) => tag !== "");
         if (tags.length < 1) {
-            throw new Error();
+            throw new Error("Add one tag at least");
         }
         if (!arrayOfStringsOnly(tags)) {
-            throw new Error();
+            throw new Error("Tags should be texts only");
         }
 
         return Object.freeze({
