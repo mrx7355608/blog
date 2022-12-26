6535d2aba33e298b48ef9358187c94a20249f735
mrx7355608 <mrx7355608@gmail.com>
2022-12-26 10:23:57 +0500

    created blogs db interactions

---
+++ src/data-access/blogs.js
@@ -0,0 +1,19 @@
+export default function buildBlogsDb({ BlogModel }) {
+    // TODO: add limit, sort, page
+    const findAll = async ({ limit, sort, skip }) =>
+        await BlogModel.find({}).sort(sort).skip(skip).limit(limit);
+    const findOne = async (filter) => await BlogModel.findOne(filter);
+    const findById = async ({ id }) => await BlogModel.findById(id);
+    const insert = async ({ blog }) => await BlogModel.create(blog);
+    const update = async (id, changes) =>
+        await BlogModel.findByIdAndUpdate(id, changes, { new: true });
+    const remove = async ({ id }) => await BlogModel.findByIdAndDelete(id);
+    return {
+        findAll,
+        findOne,
+        findById,
+        insert,
+        update,
+        remove,
+    };
+}
--- src/use-cases/blogs/index.js
+++ src/use-cases/blogs/index.js
@@ -1,10 +1,12 @@
-import buildListBlogs from "./list-blogs";
-import buildAddBlogs from "./add-blog";
-import buildEditBlogs from "./edit-blog";
-import buildUnPublishBlogs from "./unPublish-blog";
-import buildRemoveBlogs from "./remove-blog";
+import buildListBlogs from "./list-blogs.js";
+import buildAddBlogs from "./add-blog.js";
+import buildEditBlogs from "./edit-blog.js";
+import buildUnPublishBlogs from "./unPublish-blog.js";
+import buildRemoveBlogs from "./remove-blog.js";
+import buildBlogsDb from "../../data-access/blogs.js";
+import BlogModel from "../../entities/blogs/blog.model.js";
 
-const blogsDb = ""; // TODO: Add data access layer for blogs
+const blogsDb = buildBlogsDb({ BlogModel });
 const listBlogs = buildListBlogs({ blogsDb });
 const addBlogs = buildAddBlogs({ blogsDb });
 const editBlogs = buildEditBlogs({ blogsDb });
