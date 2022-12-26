865ff479512462bccaa7994d35c161c802609c99
mrx7355608 <mrx7355608@gmail.com>
2022-12-26 10:58:50 +0500

    added express.json and express.urlencoded mdlwrs

--- src/app.js
+++ src/app.js
@@ -13,6 +13,8 @@ app.use(helmet());
 app.use(cors());
 app.use(hpp());
 app.use(mongoSanitize());
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/v1/blogs", blogRouter);
