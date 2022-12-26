84b8e0f3725fc089c3d18ea3b662710043d7de35
mrx7355608 <mrx7355608@gmail.com>
2022-12-26 08:32:33 +0500

    refactored

--- db/db.js
+++ db/db.js
@@ -1,11 +1,12 @@
 import mongoose from "mongoose";
 
-export function connectToDatabase(database_url) {
-    mongoose.connect(database_url);
-    mongoose.connection.on("error", () => {
-        console.log("Error in database connection ");
-    });
-    mongoose.connection.on("connected", () => {
-        console.log("Database connected ");
-    });
+export async function connectToDatabase(database_url) {
+    try {
+        mongoose.set("strictQuery", false);
+        await mongoose.connect(database_url);
+        console.log("Connected to database");
+    } catch (err) {
+        console.log("[ERROR] There was an interruption in Database connection");
+        process.exit(1);
+    }
 }
--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 import "dotenv/config";
 import app from "./app.js";
 import config from "../config/index.js";
-// import { connectToDatabase } from "../db/db.js";
+import { connectToDatabase } from "../db/db.js";
 
 const { port } = config;
 
-function startApplication() {
-    // connectToDatabase(config.databaseUrl);
+async function startApplication() {
+    await connectToDatabase(config.databaseUrl);
     app.listen(port, () => {
         console.log("express application started on port", port);
     });
