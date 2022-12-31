5ef340c909d343e2a0c7d3138019f2b27d2821e2
mrx7355608 <mrx7355608@gmail.com>
2022-12-31 20:17:48 +0500

    added missing controllers in admin router

--- src/controllers/admin-blogs/index.js
+++ src/controllers/admin-blogs/index.js
@@ -3,23 +3,38 @@ import buildPatchUnPublishBlog from "../admin-blogs/patch-unPublish-blog.js";
 import buildUpdateBlog from "../admin-blogs/patch-blog.js";
 import buildCreateBlog from "../admin-blogs/create-blog.js";
 import buildPatchPublishBlog from "../admin-blogs/patch-publish-blog.js";
+import buildGetOneBlog from "../blogs/get-one-blog.js";
 // Blog services
 import blogServices from "../../use-cases/blogs/index.js";
-const { addBlogs, editBlogs, unPublishBlogs, publishBlogs, removeBlogs } =
-    blogServices;
+import buildGetBlogsForAdmin from "./admin-get-blogs.js";
+const {
+    listBlogs,
+    listOneBlog,
+    addBlogs,
+    editBlogs,
+    unPublishBlogs,
+    publishBlogs,
+    removeBlogs,
+} = blogServices;
 
+const getBlogs = buildGetBlogsForAdmin({ listBlogs });
+const getOneBlog = buildGetOneBlog({ listOneBlog });
 const createBlog = buildCreateBlog({ addBlogs });
 const updateBlogs = buildUpdateBlog({ editBlogs });
 const patchUnPublishBlog = buildPatchUnPublishBlog({ unPublishBlogs });
 const deleteBlog = buildDeleteBlog({ removeBlogs });
 const patchPublishBlog = buildPatchPublishBlog({ publishBlogs });
+const patchBlog = buildUpdateBlog({ editBlogs });
 
 const adminBlogController = {
+    getBlogs,
+    getOneBlog,
     createBlog,
     updateBlogs,
     patchPublishBlog,
     deleteBlog,
     patchUnPublishBlog,
+    patchBlog,
 };
 
 export default adminBlogController;
