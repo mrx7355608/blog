f473b87406aae70beb3ab7f10bba0b58d20606a3
mrx7355608 <mrx7355608@gmail.com>
2022-12-31 22:57:13 +0500

    changed back the order of validations

--- src/entities/blogs/blog.entity.js
+++ src/entities/blogs/blog.entity.js
@@ -4,28 +4,29 @@ export default function buildBlogEntity({ sanitizeHtml, arrayOfStringsOnly }) {
         if (!title) {
             throw new Error("Blog's title is missing");
         }
-        if (!tags) {
-            throw new Error("Cannot create a blog without tags");
-        }
-        if (!body) {
-            throw new Error("Blog body is required!");
-        }
-
-        // Sanitize input
         title = sanitizeHtml(title);
-        body = sanitizeHtml(body);
-        tags = tags.map((tag) => sanitizeHtml(tag));
-        tags = tags.filter((tag) => tag !== "");
-
         if (title.length < 8) {
             throw new Error("Title should be 8 characters long at least");
         }
+
+        // Tags
+        if (!tags) {
+            throw new Error("Cannot create a blog without tags");
+        }
+        tags = tags.map((tag) => sanitizeHtml(tag));
+        tags = tags.filter((tag) => tag !== "");
         if (tags.length < 1) {
             throw new Error("Add one tag at least");
         }
         if (!arrayOfStringsOnly(tags)) {
             throw new Error("Tags should be texts only");
         }
+
+        // Body
+        if (!body) {
+            throw new Error("Blog body is required!");
+        }
+        body = sanitizeHtml(body);
 
         return Object.freeze({
             getTitle: () => title,
