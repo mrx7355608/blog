d6985ecee4032173001fcb17ce9f67a2da4c5bd3
mrx7355608 <mrx7355608@gmail.com>
2023-01-02 01:06:35 +0500

    added login and a middleware to protect routes

--- src/routes/admin.routes.js
+++ src/routes/admin.routes.js
@@ -1,10 +1,17 @@
 import { Router } from "express";
 import requestHandler from "../utils/requestHandler.js";
 import adminController from "../controllers/admin/adminController.js";
+import passport from "passport";
 
 const router = Router();
 
-// blogs route
+// Only allow authenticated requests
+router.use(function (req, res, next) {
+    if (req.url === "/login") return next();
+    if (req.user && req.user.role === "admin") return next();
+    return res.status(404).json({ error: "Page not found" });
+});
+// Blogs
 router.get("/blogs/", requestHandler(adminController.getBlogs));
 router.get("/blogs/:id", requestHandler(adminController.getOneBlog));
 router.post("/blogs/", requestHandler(adminController.createBlog));
@@ -18,7 +25,19 @@ router.patch(
     requestHandler(adminController.patchUnPublishBlog)
 );
 
-// users
+// Auth
+router.post("/login", function (req, res, next) {
+    passport.authenticate("local", function (err, user, info) {
+        if (err) return next(err);
+        if (info) return res.status(400).json({ error: info.message });
+        req.logIn(user, function (err) {
+            if (err) return next(err);
+            return res.status(200).json({ login: true });
+        });
+    })(req, res, next);
+});
+
+// Users
 router.get("/users", requestHandler(adminController.getUsers));
 router.post("/users", requestHandler(adminController.postUser));
 
