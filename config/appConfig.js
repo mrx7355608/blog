f23e0b657e8a8ff66b89ecfa11726e0923a51d09
mrx7355608 <mrx7355608@gmail.com>
2023-01-01 23:45:17 +0500

    auth setup

--- config/appConfig.js
+++ config/appConfig.js
@@ -1,4 +1,5 @@
 export default {
     port: process.env.PORT,
     databaseUrl: process.env.DATABASE_URL,
+    sessionSecret: process.env.SESSION_SECRET,
 };
--- src/app.js
+++ src/app.js
@@ -4,6 +4,11 @@ import hpp from "hpp";
 import cors from "cors";
 import morgan from "morgan";
 import mongoSanitize from "express-mongo-sanitize";
+import passport from "passport";
+import session from "express-session";
+import connectMongoDbSession from "connect-mongodb-session";
+import appConfig from "../config/appConfig";
+
 // Routers
 import blogRouter from "./routes/blog.routes.js";
 import adminRouter from "./routes/admin.routes.js";
@@ -15,12 +20,34 @@ app.use(helmet());
 app.use(cors());
 app.use(hpp());
 app.use(mongoSanitize());
+// sessions setup
+const MongoStore = connectMongoDbSession(session);
+const store = new MongoStore({
+    uri: appConfig.databaseUrl,
+    collection: "sessions",
+});
+app.use(
+    session({
+        name: "sid",
+        resave: false,
+        saveUninitialized: false,
+        secret: appConfig.sessionSecret,
+        cookie: {
+            maxAge: 24 * 60 * 3600, // 1 day
+            httpOnly: true,
+        },
+        store,
+    })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 // Routes
 app.use("/api/v1/blogs", blogRouter);
-app.use("/api/v1/admin/blogs", adminRouter);
+app.use("/api/v1/admin", adminRouter);
 
 // Catch 404
 app.use(function (req, res, next) {
