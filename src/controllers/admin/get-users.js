4450624be0abcabad045f1c02ca27daa4739504a
mrx7355608 <mrx7355608@gmail.com>
2023-01-01 23:49:24 +0500

    removed password and __v fields from user objects

--- src/controllers/admin/get-users.js
+++ src/controllers/admin/get-users.js
@@ -1,6 +1,11 @@
 export default function buildGetUsers({ listUsers }) {
     return async function () {
-        const users = await listUsers();
+        let users = await listUsers();
+        users = users.map((user) => {
+            user.password = undefined;
+            user.__v = undefined;
+            return user;
+        });
         return {
             statusCode: 201,
             body: { results: users.length, users },
