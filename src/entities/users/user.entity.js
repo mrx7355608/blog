c54d5532a2ddbda24e8ac6979bcd69d1e630181c
mrx7355608 <mrx7355608@gmail.com>
2023-01-04 20:15:34 +0500

    refactoring

--- src/entities/users/user.entity.js
+++ src/entities/users/user.entity.js
@@ -1,25 +1,24 @@
 export default function buildUserEntity({ hash, emailValidator, sanitize }) {
     return function ({ name, email, password }) {
+        name = sanitize(name);
+        email = sanitize(email);
+        password = sanitize(password);
+
         if (!name) {
             throw new Error("Name is required!");
         }
-        name = sanitize(name);
         if (name.length < 4) {
             throw new Error("Name should be 4 characters long at least");
         }
-
         if (!email) {
             throw new Error("Email is required!");
         }
-        email = sanitize(email);
         if (emailValidator(email) == false) {
             throw new Error("Invalid email");
         }
-
         if (!password) {
             throw new Error("Password is required!");
         }
-        password = sanitize(password);
         if (password.length < 10) {
             throw new Error("Password should be 10 characters long at least");
         }
