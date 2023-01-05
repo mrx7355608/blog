a1dabd092ebd47879e3eae0f614389e175835bd1
mrx7355608 <mrx7355608@gmail.com>
2023-01-06 00:51:52 +0500

    removed a dependency from blog entity

--- src/entities/blogs/buildBlogEntity.js
+++ src/entities/blogs/buildBlogEntity.js
@@ -1,9 +1,5 @@
 import buildBlogEntity from "./blog.entity.js";
 import sanitizeHtml from "sanitize-html";
 
-const arrayOfStringsOnly = (array) => {
-    return array.every((elem) => typeof elem === "string");
-};
-
-const makeBlog = buildBlogEntity({ sanitizeHtml, arrayOfStringsOnly });
+const makeBlog = buildBlogEntity({ sanitizeHtml });
 export default makeBlog;
